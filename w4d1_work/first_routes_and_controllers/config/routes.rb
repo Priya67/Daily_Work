6aadfb6013be763ff411241677ccd27d038e5ed6
Priya Mangal <mangalpriya67@gmail.com>
2017-08-21 21:28:28 -0700

    W4D1 work.

---
+++ w4d1_work/first_routes_and_controllers/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    get 'users', to: 'users#index', as: 'user_index'
+    post 'users', to: 'users#create', as: 'create_user'
+    get 'users/new', to: 'users#new', as: 'new_user'
+    get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+    get 'users/:id', to: 'users#show', as: 'show_user'
+    patch 'users/:id', to: 'users#update', as: 'update_user_by_patch'
+    put 'users/:id', to: 'users#update', as: 'update_user'
+    delete 'users/:id', to: 'users#destroy', as: 'destroy_user'
+
+end
