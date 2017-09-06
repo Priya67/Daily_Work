53e18ea91dc00cc4ea2faf121d1797f1ca51b3a5
App Academy Student <student@appacademy.io>
2017-09-05 18:35:33 -0700

    JQuery

---
+++ w6d2/towers/skeleton/js/bundle.js
@@ -0,0 +1,202 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const HanoiGame = __webpack_require__(1);
+const HanoiView = __webpack_require__(2);
+
+$( () => {
+  const rootEl = $('.hanoi');
+  const game = new HanoiGame();
+  new HanoiView(game, rootEl);
+
+  console.log("all's well!!!!!!!!!!!!!!!!!!!!you better believe it.");
+});
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+class Game {
+  constructor() {
+    this.towers = [[3, 2, 1], [], []];
+  }
+
+  isValidMove(startTowerIdx, endTowerIdx) {
+      const startTower = this.towers[startTowerIdx];
+      const endTower = this.towers[endTowerIdx];
+
+      if (startTower.length === 0) {
+        return false;
+      } else if (endTower.length == 0) {
+        return true;
+      } else {
+        const topStartDisc = startTower[startTower.length - 1];
+        const topEndDisc = endTower[endTower.length - 1];
+        return topStartDisc < topEndDisc;
+      }
+  }
+
+  isWon() {
+      // move all the discs to the last or second tower
+      return (this.towers[2].length == 3) || (this.towers[1].length == 3);
+  }
+
+  move(startTowerIdx, endTowerIdx) {
+      if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+        this.towers[endTowerIdx].push(this.towers[startTowerIdx].pop());
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  print() {
+      console.log(JSON.stringify(this.towers));
+  }
+
+  promptMove(reader, callback) {
+      this.print();
+      reader.question("Enter a starting tower: ", start => {
+        const startTowerIdx = parseInt(start);
+        reader.question("Enter an ending tower: ", end => {
+          const endTowerIdx = parseInt(end);
+          callback(startTowerIdx, endTowerIdx);
+        });
+      });
+  }
+
+  run(reader, gameCompletionCallback) {
+      this.promptMove(reader, (startTowerIdx, endTowerIdx) => {
+        if (!this.move(startTowerIdx, endTowerIdx)) {
+          console.log("Invalid move!");
+        }
+
+        if (!this.isWon()) {
+          // Continue to play!
+          this.run(reader, gameCompletionCallback);
+        } else {
+          this.print();
+          console.log("You win!");
+          gameCompletionCallback();
+        }
+      });
+  }
+}
+
+module.exports = Game;
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+class View {
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+
+    this.$el.append(this.setupTowers());
+  }
+
+  setupTowers() {
+    const $tower1 = $("<ul></ul>");
+    $tower1.css("width","200px");
+
+
+    const $plateOne = $("<li></li>");
+    const $plateTwo = $("<li></li>");
+    const $plateThree = $("<li></li>");
+    const $plateFour = $("<li></li>");
+
+    $plateFour.css("width", "200px");
+    $plateThree.css("width", "200px");
+    $plateTwo.css("width", "100px");
+    $plateOne.css("width", "50px");
+
+    $tower1.prepend($plateFour);
+    $plateFour.addClass("base");
+
+
+    [$plateThree, $plateTwo, $plateOne].forEach((el) => {
+      el.addClass("gray");
+      // el.css("list-style-type", "none");
+      $tower1.prepend(el);
+    });
+
+    return $tower1;
+  }
+
+}
+
+ module.exports = View;
+
+
+/***/ })
+/******/ ]);
